4101f9bd8e7a54986a6b8546eb6924c8c4f30fc8
chio1 <36753740+chio1@users.noreply.github.com>
2019-07-11 23:43:37 +0900

    Update elysium.py

--- elysium.py
+++ elysium.py
@@ -14,8 +14,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    role = discord.utils.get(message.guild.roles , name= "뉴비도우미")
     if message.content.startswith("!명령어"):
-        await message.channel.send(embed=discord.Embed(title="제작중", description= " \n\n\n차후 추가 예정", color=0x00ff00))
+        await message.channel.send(embed=discord.Embed(title="명령어", description= " \n\n\n차후 추가 예정", color=0x00ff00))
+
+    if message.content.startswith("!인증"):
+        author = message.guild.get_member(int(message.content[4:22]))
+        role = discord.utils.get(message.guild.roles , name = "인증 회원")
+        await author.add_roles(role)
+        await message.channel.send(embed=discord.Embed(title="명령어",
+                                                       description=" 인증완료입니다",
+                                                       color=0x00ff00))
+
+    if message.content.startswith("!취소인증"):
+        author = message.guild.get_member(int(message.content[6:24]))
+        role = discord.utils.get(message.guild.roles, name="인증 회원")
+        await author.remove_roles(role)
+        await message.channel.send(embed=discord.Embed(title="명령어",
+                                                       description=" 인증취소입니다.",
+                                                       color=0x00ff00))
 
 
 access_token = os.environ["BOT_TOKEN"]
